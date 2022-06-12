91cbcee45371a88cf457f66b93b7d4ecea3b99ff
Mouthhan <50419632+Mouthhan@users.noreply.github.com>
2022-06-12 17:56:45 +0800

    Update get_model.sh

--- get_model.sh
+++ get_model.sh
@@ -1,7 +1,7 @@
 ## Download Final Ensemble's 6 Model From Dropbox
-wget "https://www.dropbox.com/s/apvqtxp701ieqe7/Deeplabv3P_resnet50_batch16.pth?dl=1" -O './models/Deeplabv3P_resnet50_batch16.pth'
-wget "https://www.dropbox.com/s/2f2r6so24v30kjw/Deeplabv3P_resnet101_batch12.pth?dl=1" -O './models/Deeplabv3P_resnet101_batch12.pth'
-wget "https://www.dropbox.com/s/np4hjs0kjrh1rr2/Deeplabv3P_resnet152_batch8.pth?dl=1" -O './models/Deeplabv3P_resnet152_batch8.pth'
-wget "https://www.dropbox.com/s/0xsx2obk6cyx4b6/Deeplabv3P_resnext50_batch12.pth?dl=1" -O './models/Deeplabv3P_resnext50_batch12.pth'
-wget "https://www.dropbox.com/s/ud9la0c1q6b1jrc/Deeplabv3P_resnext101_batch8.pth?dl=1" -O './models/Deeplabv3P_resnext101_batch8.pth'
-wget "https://www.dropbox.com/s/woduzwkblfr8wan/Deeplabv3P_se_resnet50_batch12.pth?dl=1" -O './models/Deeplabv3P_se_resnet50_batch12.pth'
+wget "https://www.dropbox.com/s/apvqtxp701ieqe7/Deeplabv3P_resnet50_batch16.pth?dl=1" -O './Models/Deeplabv3P_resnet50_batch16.pth'
+wget "https://www.dropbox.com/s/2f2r6so24v30kjw/Deeplabv3P_resnet101_batch12.pth?dl=1" -O './Models/Deeplabv3P_resnet101_batch12.pth'
+wget "https://www.dropbox.com/s/np4hjs0kjrh1rr2/Deeplabv3P_resnet152_batch8.pth?dl=1" -O './Models/Deeplabv3P_resnet152_batch8.pth'
+wget "https://www.dropbox.com/s/0xsx2obk6cyx4b6/Deeplabv3P_resnext50_batch12.pth?dl=1" -O './Models/Deeplabv3P_resnext50_batch12.pth'
+wget "https://www.dropbox.com/s/ud9la0c1q6b1jrc/Deeplabv3P_resnext101_batch8.pth?dl=1" -O './Models/Deeplabv3P_resnext101_batch8.pth'
+wget "https://www.dropbox.com/s/woduzwkblfr8wan/Deeplabv3P_se_resnet50_batch12.pth?dl=1" -O './Models/Deeplabv3P_se_resnet50_batch12.pth'
